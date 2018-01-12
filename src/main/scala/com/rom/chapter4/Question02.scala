39dfcf4358a56c5cc559a6d47629fc81345e1040
Romero Medeiros <romeromedeiros5@gmail.com>
2018-01-12 08:59:23 -0300

    Chapter 04 Question 03

--- src/main/scala/com/rom/chapter4/Question02.scala
+++ src/main/scala/com/rom/chapter4/Question02.scala
@@ -3,7 +3,7 @@ package com.rom.chapter4
 import java.io.File
 import java.util.Scanner
 
-//Write a program that reads words from a file. Use a mutable map to count
+//2. Write a program that reads words from a file. Use a mutable map to count
 //how often each word appears. To read the words, simply use a java.util.Scanner:
 //val in = new java.util.Scanner(new java.io.File("myfile.txt"))
 //while (in.hasNext()) process in.next()
---
+++ src/main/scala/com/rom/chapter4/Question03.scala
@@ -0,0 +1,15 @@
+package com.rom.chapter4
+
+import java.io.File
+import java.util.Scanner
+
+//3. Repeat the preceding exercise with an immutable map.
+object Question03 extends App {
+  val filePath: String = "C:\\Users\\rmeireles\\Desktop\\lorem.txt"
+  val in = new Scanner(new File(filePath))
+  val source = scala.io.Source.fromFile(filePath)
+  val fileContent = try source.mkString finally source.close()
+  var words = fileContent.replaceAll(",", "").split(" ")
+  val result = (for (word <- words) yield (word, words.count(_ == word))).toMap
+  result.foreach(println(_))
+}
