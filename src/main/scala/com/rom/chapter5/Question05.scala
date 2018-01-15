ecb19cc932d47b7c58e2a7839487ba85855f93c0
Romero Medeiros <romeromedeiros5@gmail.com>
2018-01-15 09:33:27 -0300

    Chapter 6 Question 05

---
+++ src/main/scala/com/rom/chapter5/Question05.scala
@@ -0,0 +1,13 @@
+package com.rom.chapter5
+
+//5. Write a Scala application, using the App trait, that prints its command-line
+//arguments in reverse order, separated by spaces. For example, scala Reverse
+//Hello World should print World Hello.
+object Question05 extends App {
+  def printInReverseOrder(sentence: String): Unit = {
+    val reversedWords: Array[String] = sentence.split(" ").reverse
+    reversedWords.foreach { println }
+  }
+
+  printInReverseOrder("Write a Scala application, using the App trait, that prints its command-line\n//arguments in reverse order, separated by spaces.")
+}
